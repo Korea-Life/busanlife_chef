a3841aeea7a8a1dba700a150fd45cd666bb8dfb6
Kyunghwan Abel Bae <Abel@Abel-ui-MacBook-Pro.local>
2016-09-25 16:38:42 +0900

    update

--- uploads/recipes/default.rb
+++ uploads/recipes/default.rb
@@ -1,5 +1,5 @@
 directory "/srv/www/busanlife/wp-content/current/uploads" do
-  mode '0744'
+  mode 0744
   action :create
   owner 'deploy'
   group 'www-data'
--- wpcontent/recipes/default.rb
+++ wpcontent/recipes/default.rb
@@ -1,5 +1,5 @@
 directory "/srv/www/busanlife/current/wp-content" do
-  mode '0744'
+  mode 0744
   action :create
   owner 'deploy'
   group 'www-data'
